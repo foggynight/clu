88f5852a75882b65a2a22a780fe860cfc3eda7f9
foggynight <foggynight709@gmail.com>
2020-10-28 01:05:39 -0230

    Update repair-permissions.sh
    repair-permissions.sh
    - Add program description
    - Add octal modifier to chmod mode arguments
    
    Added a program description in the form of a comment and added the octal
    modifier to the chmod mode arguments for clarity.

--- repair-permissions.sh
+++ repair-permissions.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
-find . -type d -print0 | xargs -0 chmod 755
-find . -type f -print0 | xargs -0 chmod 644
-find . -type f -regex ".*\.sh" -print0 | xargs -0 chmod 755
+
+# Recursively update the permissions of all files
+# in the current directory.
+#
+# Default
+# - Directories: 0755
+# - Files:       0644
+#
+# Filetypes
+# - *.sh:        0755
+
+find . -type d -print0 | xargs -0 chmod 0755
+find . -type f -print0 | xargs -0 chmod 0644
+find . -type f -regex ".*\.sh" -print0 | xargs -0 chmod 0755
